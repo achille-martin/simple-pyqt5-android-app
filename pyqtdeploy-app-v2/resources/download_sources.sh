b885414e2066ce45d2ad60efe66ff6c0539b0e15
Achille Martin <achille.martin95@gmail.com>
2023-06-03 16:36:29 +0100

    Added attempt at pyqtdeploy v3.1.2 but to no avail

---
+++ pyqtdeploy-app-v2/resources/download_sources.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+wget https://download.qt.io/archive/qt/5.15/5.15.2/single/qt-everywhere-src-5.15.2.tar.xz
+wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tar.xz
+wget https://files.pythonhosted.org/packages/c0/86/6b60c6a1b1d10524ed50fc4c6a2989492512bd46292a711318a53b243774/PyQt5_sip-12.9.1.tar.gz
+wget https://files.pythonhosted.org/packages/3b/27/fd81188a35f37be9b3b4c2db1654d9439d1418823916fe702ac3658c9c41/PyQt5-5.15.6.tar.gz
+wget https://files.pythonhosted.org/packages/33/e9/27730ac17713c0a80d81d8f3bb56213f1549d96f9dc183fd16a7eec6287c/sip-5.5.0.tar.gz
